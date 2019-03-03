7ad0db57447a6d57e2587cde1059444d6e19b6e7
HenSh <hensh2@gmail.com>
2019-03-03 20:02:26 +0200

    First

---
+++ HttpRequester/HttpRequester/HttpRequester.h
@@ -0,0 +1,19 @@
+//
+//  HttpRequester.h
+//  HttpRequester
+//
+//  Created by Hen Shabat on 03/03/2019.
+//  Copyright © 2019 Hen Shabat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HttpRequester.
+FOUNDATION_EXPORT double HttpRequesterVersionNumber;
+
+//! Project version string for HttpRequester.
+FOUNDATION_EXPORT const unsigned char HttpRequesterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HttpRequester/PublicHeader.h>
+
+
---
+++ HttpRequester/HttpRequester/HttpRequester.swift
@@ -0,0 +1,22 @@
+//
+//  HttpRequester.swift
+//  HttpRequester
+//
+//  Created by Hen Shabat on 03/03/2019.
+//  Copyright © 2019 Hen Shabat. All rights reserved.
+//
+
+import Foundation
+
+public class HttpRequester {
+    
+    private init() {}
+    
+    public static let shared: HttpRequester = HttpRequester()
+    
+    public func request() {
+        print("request")
+    }
+    
+}
+
